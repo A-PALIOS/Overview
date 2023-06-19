cb645fd617bae3c5d1a7773bf913ebc70cc26ebf
A-PALIOS <paliosadonis6@gmail.com>
2023-06-19 17:29:54 +0300

    message

--- pages/app.py
+++ pages/app.py
@@ -35,7 +35,32 @@ def main():
         st.session_state.submitted = False
 
 
-    
+    st.set_page_config(
+            page_title="Hello",
+            page_icon="👋",
+        )
+
+    st.write("# Welcome to Streamlit! 👋")
+
+    st.sidebar.success("Select a demo above.")
+
+    st.markdown(
+        """
+        Streamlit is an open-source app framework built specifically for
+        Machine Learning and Data Science projects.
+        **👈 Select a demo from the sidebar** to see some examples
+        of what Streamlit can do!
+        ### Want to learn more?
+        - Check out [streamlit.io](https://streamlit.io)
+        - Jump into our [documentation](https://docs.streamlit.io)
+        - Ask a question in our [community
+            forums](https://discuss.streamlit.io)
+        ### See more complex demos
+        - Use a neural net to [analyze the Udacity Self-driving Car Image
+            Dataset](https://github.com/streamlit/demo-self-driving)
+        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    """
+    )
 
     # Define the sidebar form
     with st.sidebar.form("my_sidebar_form"):
@@ -46,32 +71,7 @@ def main():
         
 
 
-        st.set_page_config(
-            page_title="Hello",
-            page_icon="👋",
-        )
-
-        st.write("# Welcome to Streamlit! 👋")
-
-        st.sidebar.success("Select a demo above.")
-
-        st.markdown(
-            """
-            Streamlit is an open-source app framework built specifically for
-            Machine Learning and Data Science projects.
-            **👈 Select a demo from the sidebar** to see some examples
-            of what Streamlit can do!
-            ### Want to learn more?
-            - Check out [streamlit.io](https://streamlit.io)
-            - Jump into our [documentation](https://docs.streamlit.io)
-            - Ask a question in our [community
-                forums](https://discuss.streamlit.io)
-            ### See more complex demos
-            - Use a neural net to [analyze the Udacity Self-driving Car Image
-                Dataset](https://github.com/streamlit/demo-self-driving)
-            - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-        """
-        )
+        
 
 
 
