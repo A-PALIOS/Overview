7cebc77adeeba62b4706a6a0eb008dff140e9cb4
A-PALIOS <paliosadonis6@gmail.com>
2023-06-20 14:40:39 +0300

    message

---
+++ pages/3_users-overview.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import mysql.connector
+
+
+def main():
+    st.write("## date2222 range Form")
+
+    st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+    )
+
+    st.write("# Welcome to CMT Project Management Dashboard! 👋")
+
+    st.sidebar.success("Select a demo above.")
+
+if __name__ == '__main__':
+    main()
